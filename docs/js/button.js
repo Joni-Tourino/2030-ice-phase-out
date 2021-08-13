4d8d51442befa8e0bbb1a3b3ddea0a8854f6d726
Joni Touriño <joni.eskobar@gmail.com>
2021-08-13 20:29:08 +0100

    commit

---
+++ docs/js/button.js
@@ -0,0 +1,9 @@
+
+function myButton2() {
+    var x = document.getElementById("divbutton2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
